1613a1fc21c8d36e6bea8d130a703e2ae7768e39
AuroraBlachat2 <erickariuki52@gmail.com>
2023-02-16 07:10:07 -0800

    First Code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * Main Entry
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	/* string*/
+	char Mystr[] = """Programming is like building a multilingual puzzle";
+
+	puts(Myst);
+return (0);
+}
