9800f69682c99e2fa5be4e5180cc5cd72b7a5526
AuroraBlachat2 <erickariuki52@gmail.com>
2023-02-16 07:19:06 -0800

    Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
-	/* string*/
-	char Mystr[] = """Programming is like building a multilingual puzzle";
 
+	char Mystr[] = ("\"Programming is like building a multilingual puzzle");
 	puts(Mystr);
 return (0);
+
 }
