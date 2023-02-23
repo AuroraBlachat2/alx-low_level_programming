aa548dda416cf2698e230321e6da64b17a850423
AuroraBlachat2 <erickariuki52@gmail.com>
2023-02-23 07:13:21 -0800

    +-

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * main - entry point
- * Return : 0
+ * main - tests function that prints for either positive or negative
+ * Return:0
  */
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
-
 	return (0);
 }
