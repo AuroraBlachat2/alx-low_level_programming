3d3ef07aee4591906338269cbf353a3b5a09cb63
AuroraBlachat2 <erickariuki52@gmail.com>
2023-03-07 12:33:28 -0800

    search for a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - function finds the first occurrence of the substring needle
+ * @haystack:collection of strings
+ * @needle:string to be collected
+ * Return: Null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+
+			b++;
+		}
+
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}
