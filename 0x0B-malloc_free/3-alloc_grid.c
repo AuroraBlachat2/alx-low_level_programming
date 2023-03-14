2ccef6c9d3bcae3ec4c53d908990ae13ba3689fd
AuroraBlachat2 <erickariuki52@gmail.com>
2023-03-14 07:34:57 -0700

    SD array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include"main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - prints 2 dimensional array
+ * @width: width of the array
+ * @height:height of the array
+ * Return:NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	i = j = 0;
+	if (height < 1)
+		return (NULL);
+	p = (int **)malloc(height * sizeof(p));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(p[j]);
+			free(p);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+}
